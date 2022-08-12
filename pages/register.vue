de45154f05be13c2792ebe0e544a41fd0d91c40a
nardos atnaw <nardidumessa@gmail.com>
2022-08-12 10:19:57 +0300

    register form link added

--- pages/register.vue
+++ pages/register.vue
@@ -4,26 +4,26 @@
     <div class="social-media">
       <img class="bg-back" src="../assets/img/texture.svg" alt="" />
       <div class="soc-links">
-
-          <a href="https://www.facebook.com/beyonddancers">
-            <img src="../assets/img/facebookwhite.svg" alt="">
-          </a>
-             <a href="https://www.instagram.com/beyonddancers/">
-            <img src="../assets/img/instawhite.svg" alt="">
-          </a>
-             <a href="https://www.youtube.com/channel/UC53ryMIGEZptZToOvmBgLWQ">
-            <img src="../assets/img/youtubewhite.svg" alt="">
-          </a>
-              <a href="https://www.tiktok.com/@beyonddancestudio">
-            <img src="../assets/img/tiktokwhite.svg" alt="">
-          </a>
+        <a href="https://www.facebook.com/beyonddancers">
+          <img src="../assets/img/facebookwhite.svg" alt="" />
+        </a>
+        <a href="https://www.instagram.com/beyonddancers/">
+          <img src="../assets/img/instawhite.svg" alt="" />
+        </a>
+        <a href="https://www.youtube.com/channel/UC53ryMIGEZptZToOvmBgLWQ">
+          <img src="../assets/img/youtubewhite.svg" alt="" />
+        </a>
+        <a href="https://www.tiktok.com/@beyonddancestudio">
+          <img src="../assets/img/tiktokwhite.svg" alt="" />
+        </a>
       </div>
-
     </div>
     <div class="register-form row-max-grid">
       <div class="container row-max-grid">
-        <h1 >Beyond Dance Pre-Registration Form</h1>
-        <p class="row-grid">REGISTER YOUR CHILD TODAY! - Limited Space Available</p>
+        <h1>Beyond Dance Pre-Registration Form</h1>
+        <p class="row-grid">
+          REGISTER YOUR CHILD TODAY! - Limited Space Available
+        </p>
         <p class="row-grid">
           *Due to COVID capacity constraints registration is not fully confirmed
           until pre-registration form is completed and registration is payed in
@@ -39,22 +39,27 @@
           </p>
           <input class="input" type="text" />
           <p>Student's Gender *</p>
-          <p><input type="radio" name="gender" value="female" class="mg-left"/> Female</p> 
-          <p><input type="radio" class="mg-left" name="gender" value="male" /> Male</p>
+          <p>
+            <input type="radio" name="gender" value="female" class="mg-left" />
+            Female
+            <input type="radio" class="mg-left" name="gender" value="male" />
+            Male
+          </p>
+          <p></p>
           <p>Date of Birth</p>
           <input class="input" type="date" />
           <p>Age Group *</p>
-            <p>
-          <input type="radio" class="mg-left"/>
-         3-5 year old</p>
- <p>
-          <input type="radio" class="mg-left" />
-          6-9 year old</p> 
-<p> 
-          <input type="radio" class="mg-left" />10-13 year old</p>  <p>
-          <input type="radio" class="mg-left" />14-18 year old</p> 
-           <p>
-          <input type="radio" class="mg-left" />Other</p>
+          <p>
+            <input type="radio" class="mg-left" />
+            3-5 year old <input type="radio" class="mg-left" /> 6-9 year old
+          </p>
+
+          <p></p>
+          <p>
+            <input type="radio" class="mg-left" />10-13 year old
+            <input type="radio" class="mg-left" />14-18 year old
+          </p>
+          <p><input type="radio" class="mg-left" />Other</p>
           <p>School (if applicable)</p>
           <input class="input" type="text" />
           <p>Email Address *</p>
@@ -62,35 +67,46 @@
           <p>Phone Number *</p>
           <input class="input" type="text" />
           <p>Instagram Account (if applicable)</p>
-          <input class="input" type="text" /> <p>
-          What are you interested in? (Check all that applies) *</p>
+          <input class="input" type="text" />
+          <p>What are you interested in? (Check all that applies) *</p>
           <p>
-          <input type="checkbox" class="mg-left"> Children's Dance Classes (Ballet, Jazz, Hip Hop, Lyrical Etc.)</p> 
+            <input type="checkbox" class="mg-left" /> Children's Dance Classes
+            (Ballet, Jazz, Hip Hop, Lyrical Etc.)
+          </p>
+
           <p>
-          <input type="checkbox" class="mg-left"/> Taekwondo</p> 
+            <input type="checkbox" class="mg-left" />Adults Classes (Yoga,
+            Kickboxing,Salsa, Zumba...)
+          </p>
           <p>
-          <input type="checkbox" class="mg-left"/>Private Piano Lessons </p> 
-          <p><input type="checkbox" class="mg-left" /> Summer Camp</p>  
-          <p><input type="checkbox" class="mg-left" />Birthday
-           Parties</p>  
-          <p><input type="checkbox" class="mg-left" /> Adults Classes (Yoga,
-          Kickboxing,Salsa, Zumba...)</p>  
-         <p> <input type="checkbox" class="mg-left" />   Studio Space
-        Rental</p>  
-         <p> <input type="checkbox" class="mg-left" /> Job Opportunity</p> 
-         <p> <input type="checkbox" class="mg-left" /> Other</p>
-    <p>Dancer's Shoe Size (Euro Measurements)</p>
+            <input type="checkbox" class="mg-left" /> Taekwondo<input
+              type="checkbox"
+              class="mg-left"
+            />
+            Summer Camp
+          </p>
+          <p>
+            <input type="checkbox" class="mg-left" />Birthday Parties
+            <input type="checkbox" class="mg-left" /> Private Piano Lessons
+          </p>
+          <p></p>
+          <p>
+            <input type="checkbox" class="mg-left" /> Studio Space Rental
+            <input type="checkbox" class="mg-left" /> Job Opportunity
+          </p>
+          <p></p>
+          <p><input type="checkbox" class="mg-left" /> Other</p>
+          <p>Dancer's Shoe Size (Euro Measurements)</p>
           <input class="input" type="text" />
           <p>Additional Information</p>
           <input class="input" type="text" />
-
         </form>
         <button class="btn btn-thrid">Submit</button>
       </div>
     </div>
-      <div class="big-image row-grid">
-          <img src="../assets/img/showcase 2.jpg" alt="">
-        </div>
+    <div class="big-image row-grid">
+      <img src="../assets/img/showcase 2.jpg" alt="" />
+    </div>
     <Footer />
   </div>
 </template>
