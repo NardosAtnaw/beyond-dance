dcb74354a2bf8b7a9b335917670755be79541858
nardos atnaw <nardidumessa@gmail.com>
2022-08-13 12:25:41 +0300

    deploy1

--- components/Navlink.vue
+++ components/Navlink.vue
@@ -3,11 +3,21 @@
     <nav class="home-nav">
         <div class="container">
 
-            <div class="logo">
-                <img src="../assets/img/logopng.png" alt="">
+            <div class="logo"> <nuxt-link to="/">
+    <img src="../assets/img/logopng.png" alt="">
+    <!-- <div class="logo-img"></div>
+    <div class="logo-mask"></div> -->
+    </nuxt-link>
             </div>
-
+       <div class="mobile-link">
+                        <ul class="ul-links">
+              
+                  <li ><nuxt-link to="/register"><span class="box-form">Register Now</span> </nuxt-link></li>
+            </ul>
+                </div>
             <div class="links">
+         
+
             <ul class="ul-links">
                 <li>  <nuxt-link to="/">Home</nuxt-link></li>
               <li><nuxt-link to="/">Dance Studio</nuxt-link></li>
@@ -15,7 +25,7 @@
                   <li><nuxt-link to="/">Performance Information and Videos</nuxt-link></li>
                   <li><nuxt-link to="/">Events</nuxt-link></li>
                      <li><nuxt-link to="/">Contact Us</nuxt-link></li>
-                  <li ><nuxt-link to="/"><span class="box-form">Register Now</span> </nuxt-link></li>
+                  <li ><nuxt-link to="/register"><span class="box-form">Register Now</span> </nuxt-link></li>
             </ul>
             </div>
         </div>
--- pages/index.vue
+++ pages/index.vue
@@ -30,171 +30,209 @@
           </div>
           <div class="who-desc">
             <p>
-
-              Beyond Dance Studio is the first of its kind in Ethiopia offering a
-              wide range of kids to young adult dance classes from ballet, jazz,
-              hip hop, technique and beyond. We as a company are committed to grow
-              your dancers at an accelerated rate through dedication and
+              Beyond Dance Studio is the first of its kind in Ethiopia offering
+              a wide range of kids to young adult dance classes from ballet,
+              jazz, hip hop, technique and beyond. We as a company are committed
+              to grow your dancers at an accelerated rate through dedication and
               discipline. Beyond Dance’s vision is to introduce the joy of dance
-              to hundreds of children across Addis Ababa. When you decide to join
-              us at Beyond Dance, you too will be able to take advantage of our
-              professional staff at our fully-equipped and recently renovated
-              studio. Whether you’re a beginner or an experienced dancer looking
-              to improve your technique, we guarantee you’ll find it here.
+              to hundreds of children across Addis Ababa. When you decide to
+              join us at Beyond Dance, you too will be able to take advantage of
+              our professional staff at our fully-equipped and recently
+              renovated studio. Whether you’re a beginner or an experienced
+              dancer looking to improve your technique, we guarantee you’ll find
+              it here.
             </p>
             <!-- <button class="btn btn-secondary">Discover More</button> -->
           </div>
         </div>
-     
       </div>
     </div>
 
-       <div class="big-image row-grid">
-          <img src="../assets/img/showcase 2.jpg" alt="">
+    <div class="big-image row-grid">
+      <img src="../assets/img/showcase 2.jpg" alt="" />
+    </div>
+    <div class="programs row-max-grid">
+      <div class="container">
+        <h1>Programs</h1>
+        <div class="schedule-all row-max-grid">
+          <div class="sch-title">
+            <h2>Schedule</h2>
+            <div class="decor-box"></div>
+            <div class="sch-img row-max-grid">
+              <img src="../assets/img/unnamed (2).png" alt="" />
+              <div class="desc row-max-grid">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Dolorum voluptatum ut eaque incidunt unde repellat quos
+                  explicabo, eligendi id minima voluptates ullam natus eum neque
+                  ducimus, nisi nulla, placeat odit.
+                </p>
+                <div class="buttons">
+                  <button class="btn btn-thrid">
+                    <a href="">See Schedule</a>
+                  </button>
+                  <button class="btn btn-secondary">
+                    <a href="./register"> Register Now</a>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-<div class="programs row-max-grid">
-  <div class="container">
-    <h1>Programs</h1>
-    <div class="schedule-all row-max-grid">
-      <div class="sch-title">
-        <h2>Schedule</h2>
-        <div class="decor-box"> </div>
-        <div class="sch-img row-max-grid">
-          <img src="../assets/img/unnamed (2).png" alt="">
-          <div class="desc row-max-grid">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum voluptatum ut eaque incidunt unde repellat quos explicabo, eligendi id minima voluptates ullam natus eum neque ducimus, nisi nulla, placeat odit.</p>
-            <div class="buttons">
-              <button class="btn btn-thrid"><a href="">See Schedule</a> </button>
-               <button  class="btn btn-secondary"><a href="./register">
-                Register Now</a></button>
+        <div class="class-all row-max-grid">
+          <div class="class-title">
+            <h2>Classes</h2>
+            <div class="decor-box"></div>
+            <div class="class-list">
+              <div class="class-single row-max-grid">
+                <img src="../assets/img/takwalogo.svg" alt="" />
+                <h3 class="row-grid">TAEKWONDO</h3>
+                <p class="row-grid">
+                  Taekwondo is a system of symmetrical body exercises or
+                  techniques designed for self-defense and counterattack in
+                  unarmed combat
+                </p>
+              </div>
+              <div class="class-single row-max-grid">
+                <img src="../assets/img/hiphoplogo.svg" alt="" />
+                <h3 class="row-grid">HIP HOP</h3>
+                <p class="row-grid">
+                  Hip Hop is very unique, as it allows its dancers to perform
+                  with freedom of movement, adding in their own personalities.
+                </p>
+              </div>
+              <div class="class-single row-max-grid">
+                <img src="../assets/img/gymlogo.svg" alt="" />
+                <h3 class="row-grid">GYMNASTICS</h3>
+                <p class="row-grid">
+                  Gymnastics is a sport that includes physical exercises
+                  requiring balance, strength, flexibility, agility,
+                  coordination, dedication and endurance.
+                </p>
+              </div>
+              <div class="class-single row-max-grid">
+                <img src="../assets/img/yogalogo.svg" alt="" />
+                <h3 class="row-grid">YOGA</h3>
+                <p class="row-grid">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Necessitatibus, veniam! Commodi atque laboriosam officia.
+                  Doloribus animi perspiciatis nam, quod hic debitis .
+                </p>
+              </div>
+              <div class="class-single row-max-grid">
+                <img src="../assets/img/balletlogo.svg" alt="" />
+                <h3 class="row-grid">BALLET</h3>
+                <p class="row-grid">
+                  The focus will be set on placement, alignment, flexibility and
+                  musicality through a blend of traditional ballet techniques.
+                  As students’ progress through the levels
+                </p>
+              </div>
+              <div class="class-single row-max-grid">
+                <img src="../assets/img/painologo.svg" alt="" />
+                <h3 class="row-grid">PAINO</h3>
+                <p class="row-grid">
+                  Students learn basic piano skills and fundamentals of music
+                  literacy, including basic notation, rhythms, time signatures,
+                  and intervals.
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-
     </div>
-        <div class="class-all row-max-grid">
-      <div class="class-title">
-        <h2>Classes</h2>
-        <div class="decor-box"></div>
-        <div class="class-list">
+    <div class="show-case row-max-grid">
+      <div class="container">
+        <h1>Show Case 2022</h1>
 
-          <div class="class-single row-max-grid">
-            <img src="../assets/img/takwalogo.svg" alt="">
-            <h3 class=" row-grid">TAEKWONDO</h3>
-            <p class=" row-grid">Taekwondo is a system of symmetrical body exercises or techniques designed for self-defense and counterattack in unarmed combat</p>
+        <div class="show-videos row-grid">
+          <div class="single-video">
+            <iframe
+              src="https://www.youtube.com/embed/hZsczpTkXj8"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
           </div>
-       <div class="class-single row-max-grid">
-            <img src="../assets/img/hiphoplogo.svg" alt="">
-            <h3 class=" row-grid">HIP HOP</h3>
-            <p class=" row-grid">Hip Hop is very unique, as it allows its dancers to perform with freedom of movement, adding in their own personalities. </p>
+          <div class="single-video">
+            <iframe
+              src="https://www.youtube.com/embed/1G6uCoGnBRs"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
           </div>
-                 <div class="class-single row-max-grid">
-            <img src="../assets/img/gymlogo.svg" alt="">
-            <h3 class=" row-grid">GYMNASTICS</h3>
-            <p class=" row-grid">Gymnastics is a sport that includes physical exercises requiring balance, strength, flexibility, agility, coordination, dedication and endurance.</p>
-          </div>
-                 <div class="class-single row-max-grid">
-            <img src="../assets/img/yogalogo.svg" alt="">
-            <h3 class=" row-grid">YOGA</h3>
-            <p class=" row-grid">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, veniam! Commodi atque laboriosam officia. Doloribus animi perspiciatis nam, quod hic debitis .</p>
-          </div>
-                 <div class="class-single row-max-grid">
-            <img src="../assets/img/balletlogo.svg" alt="">
-            <h3 class=" row-grid">BALLET</h3>
-            <p class=" row-grid">The focus will be set on placement, alignment, flexibility and musicality through a blend of traditional ballet techniques. As students’ progress through the levels</p>
-          </div>
-                 <div class="class-single row-max-grid">
-            <img src="../assets/img/painologo.svg" alt="">
-            <h3 class=" row-grid">PAINO</h3>
-            <p class=" row-grid">Students learn basic piano skills and fundamentals of music literacy, including basic notation, rhythms, time signatures, and intervals.</p>
+          <div class="single-video">
+            <iframe
+              src="https://www.youtube.com/embed/7bRxWXRofeM"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
           </div>
         </div>
       </div>
-
     </div>
-  </div>
-</div>
-<div class="show-case row-max-grid">
-  <div class="container">
-   <h1>Show Case 2022</h1>
+    <div class="home-contact row-max-grid row-bottom-grid">
+      <div class="container">
+        <h1>Contact Us</h1>
+        <div class="contact-list">
+          <div class="form row-max-grid">
+            <form action="">
+              <p>Name</p>
+              <input class="input" type="text" />
+              <p>Email</p>
+              <input class="input" type="text" />
+              <p>Phone Number</p>
+              <input class="input" type="text" />
+              <p>Message</p>
+              <input class="input" type="text" /><br />
+              <button class="btn btn-secondary">Submit</button>
+            </form>
+          </div>
+          <div class="contact-info row-max-grid">
+            <h2 class="">Write Us Now!</h2>
+            <p class="row-grid">
+              Africa Ave, Addis Ababa, Ethiopia<br /><br />
 
-   <div class="show-videos row-grid">
-    <div class="single-video">
-      
-<iframe 
-src="https://youtu.be/hZsczpTkXj8">
-</iframe>
-    </div>
-        <div class="single-video">
-      
-<iframe
-src="https://www.youtube.com/watch?v=1G6uCoGnBRs">
-</iframe>
-    </div>
-        <div class="single-video">
-      
-<iframe
-src="https://www.youtube.com/watch?v=7bRxWXRofeM">
-</iframe>
-    </div>
-   </div>
-  </div>
-</div>
-  <div class="home-contact row-max-grid row-bottom-grid">
-    <div class="container">
-      <h1>Contact Us</h1>
-<div class="contact-list">
-  <div class="form row-max-grid">
-    <form action="">
-      <p>Name</p>
-      <input class="input" type="text">
-          <p>Email</p>
-      <input class="input" type="text">
-          <p>Phone Number</p>
-      <input class="input" type="text">
-          <p>Message</p>
-      <input class="input" type="text"><br/>
-       <button class="btn btn-secondary">Submit</button>
-    </form>
+              Beyonddancers.com<br /><br />
 
-  </div>
-  <div class="contact-info row-max-grid">
-    
-    <h2 class="">Write Us Now!</h2>
-    <p class="row-grid">Africa Ave, Addis Ababa, Ethiopia
-
-Beyonddancers.com
-
-+251965757474</p>
-<div class="home-social-links row-grid">
-       <div class="soc-links">
-        <a href="https://www.facebook.com/beyonddancers">
-          <img src="../assets/img/faceblue.svg" alt="" />
-        </a>
-        <a href="https://www.instagram.com/beyonddancers/">
-          <img src="../assets/img/instablue.svg" alt="" />
-        </a>
-        <a href="https://www.youtube.com/channel/UC53ryMIGEZptZToOvmBgLWQ">
-          <img src="../assets/img/youblue.svg" alt="" />
-        </a>
-        <a href="https://www.tiktok.com/@beyonddancestudio">
-          <img src="../assets/img/tiktokblue.svg" alt="" />
-        </a>
+              +251965757474<br /><br />
+            </p>
+            <div class="home-social-links row-max-grid">
+              <div class="soc-links">
+                <a href="https://www.facebook.com/beyonddancers">
+                  <img src="../assets/img/faceblue.svg" alt="" />
+                </a>
+                <a href="https://www.instagram.com/beyonddancers/">
+                  <img src="../assets/img/instablue.svg" alt="" />
+                </a>
+                <a
+                  href="https://www.youtube.com/channel/UC53ryMIGEZptZToOvmBgLWQ"
+                >
+                  <img src="../assets/img/youblue.svg" alt="" />
+                </a>
+                <a href="https://www.tiktok.com/@beyonddancestudio">
+                  <img src="../assets/img/tiktokblue.svg" alt="" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-</div>
-  </div>
-</div>
     </div>
-  </div>
-        <Footer />
+    <Footer />
   </div>
 </template>
 
 <script>
-import Footer from '../components/Footer.vue';
+import Footer from "../components/Footer.vue";
 export default {
-    name: "IndexPage",
-    components: { Footer }
+  name: "IndexPage",
+  components: { Footer },
 };
 </script>
