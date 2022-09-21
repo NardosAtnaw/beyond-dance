78fe118ea66a0ae985b3f6ec66a9a402ff26e3f3
nardos atnaw <nardidumessa@gmail.com>
2022-09-21 14:50:37 +0300

    Deployment Suggested update4

--- components/Contactus.vue
+++ components/Contactus.vue
@@ -7,7 +7,7 @@
       <!-- <h1>Contact Us</h1> -->
 
       <div class="contact-list">
-        <div class="form ">
+        <div class="form">
           <h2 class="big-txt">Get in touch with us!</h2>
           <h3 class="little-txt">
             Use the form below to get in touch with us, or click on the links to
@@ -31,7 +31,7 @@
             src="../assets/img/program/showcase27.jpg"
             alt=""
           />
-          <h2 class="">Write Us Now!</h2>
+          <!-- <h2 class="">Write Us Now!</h2> -->
           <p class="">
             Africa Ave, Addis Ababa, Ethiopia<br />
 
@@ -42,22 +42,18 @@
           <div class="home-social-links row-grid">
             <div class="soc-links">
               <a href="https://www.facebook.com/beyonddancers">
-                <img class="soc-img" src="../assets/img/faceblue.svg" alt="" />
+                <img class="soc-img" src="../assets/img/faceb.svg" alt="" />
               </a>
               <a href="https://www.instagram.com/beyonddancers/">
-                <img class="soc-img" src="../assets/img/instablue.svg" alt="" />
+                <img class="soc-img" src="../assets/img/instab.svg" alt="" />
               </a>
               <a
                 href="https://www.youtube.com/channel/UC53ryMIGEZptZToOvmBgLWQ"
               >
-                <img class="soc-img" src="../assets/img/youblue.svg" alt="" />
+                <img class="soc-img" src="../assets/img/youtubeb.svg" alt="" />
               </a>
               <a href="https://www.tiktok.com/@beyonddancestudio">
-                <img
-                  class="soc-img"
-                  src="../assets/img/tiktokblue.svg"
-                  alt=""
-                />
+                <img class="soc-img" src="../assets/img/tiktokb.svg" alt="" />
               </a>
             </div>
           </div>
