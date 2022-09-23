8c80e9dc8d69b5a90d06c3cc5d66fa51b18367bc
nardos atnaw <nardidumessa@gmail.com>
2022-09-23 14:11:41 +0300

    chat added

--- components/Navlink.vue
+++ components/Navlink.vue
@@ -119,6 +119,18 @@ export default {
     isOpen: false,
   },
   mounted() {
+    var Tawk_API = Tawk_API || {},
+      Tawk_LoadStart = new Date();
+    (function () {
+      var s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+      s1.async = true;
+      s1.src = "https://embed.tawk.to/632d5d1737898912e96ad5dd/1gdkj793v";
+      s1.charset = "UTF-8";
+      s1.setAttribute("crossorigin", "*");
+      s0.parentNode.insertBefore(s1, s0);
+    })();
+
     $(document).ready(function () {
       //save boolean
       var pause = false;
